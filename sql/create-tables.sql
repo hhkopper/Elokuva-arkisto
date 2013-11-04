bccb9ed39e30b15ebf537fb7e4451d8ba59e3646
Hanna H Kopperi <hhkopper@svm-19.cs.helsinki.fi>
2013-11-04 16:16:41 +0200

    taulukkoa lisää

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -3,3 +3,13 @@ idtunnus serial primary key, /* käyttäjän tunnus, jonka avulla erotetaan kayt
 kayttajatunnus varchar(15) unique not null, /* kayttajatunnus, jonka käyttäjä antaa itselleen, kellään ei voi olla samanlaista*/
 salasana varchar(15) not null /* käyttäjän salasana */
 );
+
+create table Elokuva(
+idtunnus serial primary key,
+nimi varchar(100) not null,
+numero integer,
+kesto integer,
+ikaraja integer,
+valmistusvuosi integer,
+maat varchar(100)
+);
