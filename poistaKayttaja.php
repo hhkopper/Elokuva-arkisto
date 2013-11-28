5012d087e5889ddd0b194b02d84aeb2ebd44c8e9
Hanna H Kopperi <hhkopper@svm-19.cs.helsinki.fi>
2013-11-28 20:26:34 +0200

    kayttaja poistetaan oikein

--- poistaKayttaja.php
+++ poistaKayttaja.php
@@ -5,7 +5,6 @@ require_once "kirjasto/elokuva.php";
 saakoNahdaSivun();
 
 $id = $_SESSION['kirjautunut']->getKayttajaId();
-elokuva::poistaKaikkiKayttajanElokuvat($id);
 kayttaja::poistaKayttaja($id);
 
 header('Location: logOut.php');
